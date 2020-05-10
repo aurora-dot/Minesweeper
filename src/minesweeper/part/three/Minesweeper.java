0c38b054466ad1c6299b5d08c2fe8c21f757a0c0
Esther <esther@pastel.codes>
2020-05-10 16:35:34 +0100

    Starting to dev first gui window

--- src/minesweeper/part/three/Minesweeper.java
+++ src/minesweeper/part/three/Minesweeper.java
@@ -5,12 +5,20 @@
  */
 package minesweeper.part.three;
 
+import java.io.File;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 /**
@@ -19,26 +27,64 @@ import javafx.stage.Stage;
  */
 public class Minesweeper extends Application {
     
+    private TabPane tabPane = new TabPane();
+    FileChooser fileChooser = new FileChooser();
+    Stage stage;
+    
     @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
+    public void start(Stage primaryStage) {    
+        this.stage = primaryStage;
+        BorderPane root = new BorderPane();
+
+        root.setTop(createMenus());
+
+        Scene scene = new Scene(root);
+
+        primaryStage.setTitle("Minesweeper");
         primaryStage.setScene(scene);
         primaryStage.show();
+ }
+    
+    private MenuBar createMenus() {
+        MenuBar menus = new MenuBar();
+        
+        Menu fileMenu = new Menu("File");
+        
+        MenuItem saveItem = new MenuItem("Save...");
+        saveItem.setOnAction(e -> {
+            saveMinefield();
+        });
+        
+        MenuItem openItem = new MenuItem("Open...");
+        openItem.setOnAction(e -> {
+            openMinefield();
+        });
+        
+        fileMenu.getItems().addAll(openItem, saveItem);
+        
+        Menu gameMenu = new Menu("Game");
+        
+        MenuItem newItem = new MenuItem("New...");
+        openItem.setOnAction(e -> {
+            newGame();
+        });
+        
+        gameMenu.getItems().addAll(newItem); 
+        
+        menus.getMenus().addAll(fileMenu, gameMenu);
+        return menus;
+    }
+    
+    private void saveMinefield() {
+
+    }
+        
+    private void openMinefield() {
+
+    }
+    
+    private void newGame() {
+
     }
 
     /**
